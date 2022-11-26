2940b3f32fe8edc2a91ed0121ac220a5b4af1ecf
Benac2003 <22653713+Benac2003@users.noreply.github.com>
2022-11-26 15:44:55 +0800

    added connections and threading

--- src/main.rs
+++ src/main.rs
@@ -1,48 +1,54 @@
 pub mod keys;
 pub mod mathlib;
-use crate::keys::{KeyPair, Key};
+pub mod threading;
+pub mod connection;
+
+use crate::keys::{KeyPair};
+use crate::threading::{Threads};
+
 use std::io::{self, Write, BufRead};
 
 
 fn main() {
+    let threads = Threads::new();
+    threads.connections();
+    // let pair = KeyPair::new(&32);
+    // // Test number below 3233
+    // let mut hex: u64 = 0x00000C9F;
+    // print!("Num Test 1: {:X} -> ", hex);
+    // print!("{:X} -> ", pair.pkey.encrypt64(hex));
+    // print!("{:X}    PASS\n", pair.skey.encrypt64(pair.pkey.encrypt64(hex)));
 
-    let pair = KeyPair::new(&32);
-    // Test number below 3233
-    let mut hex: u64 = 0x00000C9F;
-    print!("Num Test 1: {:X} -> ", hex);
-    print!("{:X} -> ", pair.pkey.encrypt64(hex));
-    print!("{:X}    PASS\n", pair.skey.encrypt64(pair.pkey.encrypt64(hex)));
+    // // Test number above 3233
+    // hex = 0x0036449e;
+    // print!("Num Test 2: {:X} -> ", hex);
+    // print!("{:X} -> ", pair.pkey.encrypt64(hex));
+    // print!("{:X}    FAIL\n\n", pair.skey.encrypt64(pair.pkey.encrypt64(hex)));
 
-    // Test number above 3233
-    hex = 0x0036449e;
-    print!("Num Test 2: {:X} -> ", hex);
-    print!("{:X} -> ", pair.pkey.encrypt64(hex));
-    print!("{:X}    FAIL\n\n", pair.skey.encrypt64(pair.pkey.encrypt64(hex)));
+    // // Write keys to file
+    // pair.pkey.write_to_file("rsa.pub");
+    // pair.skey.write_to_file("rsa.pem");
+    // println!("Saved keys to disk.\n");
 
-    // Write keys to file
-    pair.pkey.write_to_file("rsa.pub");
-    pair.skey.write_to_file("rsa.pem");
-    println!("Saved keys to disk.\n");
+    // // Ask for input
+    // print!("Enter text to encrypt: ");
+    // io::stdout().flush().unwrap();
+    // let mut encrypted: String = String::new();
+    // for line in io::stdin().lock().lines() {
+    //     let data: String = line.unwrap();
+    //     println!("{:?}", data);
+    //     encrypted = pair.pkey.encrypt_str(data);
+    //     println!("Result: {}", encrypted);
+    //     break;
+    // }
 
-    // Ask for input
-    print!("Enter text to encrypt: ");
-    io::stdout().flush().unwrap();
-    let mut encrypted: String = String::new();
-    for line in io::stdin().lock().lines() {
-        let data: String = line.unwrap();
-        println!("{:?}", data);
-        encrypted = pair.pkey.encrypt_str(data);
-        println!("Result: {}", encrypted);
-        break;
-    }
+    // // Test reading key from file
+    // let key_from_file: Key = Key::from_file("rsa.pem");
 
-    // Test reading key from file
-    let key_from_file: Key = Key::from_file("rsa.pem");
-
-    // Test decryption
-    println!("Decrypting result...");
-    let decrypted = key_from_file.decrypt_str(encrypted);
-    println!("Final message: \n\n{}\n", decrypted);
+    // // Test decryption
+    // println!("Decrypting result...");
+    // let decrypted = key_from_file.decrypt_str(encrypted);
+    // println!("Final message: \n\n{}\n", decrypted);
 
 }
 
